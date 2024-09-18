4b9aed95aec71c992a08e66da4d2bcd59fc52e04
David Molina <dlmo18@gmail.com>
2024-09-18 20:29:47 +0000

    Completado flujo de edición

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,22 +1,25 @@
-import React from 'react';
+import React from "react";
 
-function TodoItem({ task, deleteTask, toggleCompleted }) {
-    function handleChange() {
-        toggleCompleted(task.id);
-    }
- 
-    return (
-        <div className="todo-item">
-            <input 
-                type="checkbox"
-                checked={task.completed}
-                onChange={handleChange}
-            />
-            <p>{task.text}</p>
-            <button onClick={() => deleteTask(task.id)} title="Eliminar">
-            X
-            </button>
-        </div>
-    );
+function TodoItem({ task, editTask, deleteTask, toggleCompleted }) {
+  function handleChange() {
+    toggleCompleted(task.id);
+  }
+
+  let className = "todo-item " + (task.completed ? "" : "active");
+
+  return (
+    <div className={className}>
+      <input type="checkbox" checked={task.completed} onChange={handleChange} />
+      <p>{task.text}</p>
+      <div className="control">
+        <button onClick={() => editTask(task.id)} title="Eliminar">
+          <i className="fas fa-edit"></i>
+        </button>
+        <button onClick={() => deleteTask(task.id)} title="Eliminar">
+          <i className="fas fa-trash"></i>
+        </button>
+      </div>
+    </div>
+  );
 }
 export default TodoItem;
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,67 +1,142 @@
-import { useState } from 'react';
-import TodoItem from './TodoItem';
+import { useState } from "react";
+import TodoItem from "./TodoItem";
 
 function TodoList() {
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            text: 'Comprar pan',
-            completed: true
-        },
-        {
-            id: 2,
-            text: 'Preparar almuerzos',
-            completed: false
-        }
-    ]);
-    
-    const [text, setText] = useState('');
-    function addTask(text) {
-        const newTask = {
-            id: Date.now(),
-            text,
-            completed: false
-        };
-        setTasks([...tasks, newTask]);
-        setText('');
-    }
-   
-    function deleteTask(id) {
-        setTasks(tasks.filter(task => task.id !== id));
-    }
-    
-    function toggleCompleted(id) {
-        setTasks(tasks.map(task => {
-            if (task.id === id) {
-                return {...task, completed: !task.completed};
-            } else {
-                return task;
-            } 
-        }));
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Comprar pan",
+      completed: true,
+    },
+    {
+      id: 2,
+      text: "Preparar almuerzos",
+      completed: false,
+    },
+  ]);
+
+  const [item, setItem] = useState({
+    id: Date.now(),
+    text: "",
+    completed: false,
+  });
+
+  function addTask(item) {
+    if (item.text == "") {
+      return;
     }
 
-    return (
-        <>
-            <div className="todo-form">
-                <input
-                    className="item-name" 
-                    value={text}
-                    onChange={e => setText(e.target.value)} 
-                    />
-                <button className="item-button" onClick={() => addTask(text)}>Agregar</button>
-            </div>
-            <hr></hr>
-            <div className="todo-list">
-                {tasks.map(task => (
-                    <TodoItem
-                        key={task.id} 
-                        task={task}
-                        deleteTask={deleteTask}
-                        toggleCompleted={toggleCompleted} 
-                    />
-                ))}
-            </div>
-        </>
+    const newTask = {
+      id: item.id,
+      text: item.text,
+      completed: item.completed,
+    };
+
+    let tempTasks = [];
+
+    let isNew = true;
+    tasks.forEach((task) => {
+      if (task.id === item.id) {
+        tempTasks = [...tempTasks, newTask];
+        isNew = false;
+      } else {
+        tempTasks = [...tempTasks, task];
+      }
+    });
+
+    if (isNew) {
+      tempTasks = [...tempTasks, newTask];
+    }
+
+    setTasks(tempTasks);
+    setItem({
+      id: Date.now(),
+      text: "",
+      completed: false,
+    });
+  }
+
+  function deleteTask(id) {
+    setTasks(tasks.filter((task) => task.id !== id));
+  }
+
+  function clearTask() {
+    setItem({
+      id: Date.now(),
+      text: "",
+      completed: false,
+    });
+  }
+
+  function editTask(id) {
+    let item = {
+      id: Date.now(),
+      text: "",
+      completed: false,
+    };
+
+    tasks.forEach((task) => {
+      if (task.id === id) {
+        item = task;
+      }
+    });
+
+    setItem(item);
+  }
+
+  function toggleCompleted(id) {
+    setTasks(
+      tasks.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed: !task.completed };
+        } else {
+          return task;
+        }
+      })
     );
+  }
+
+  return (
+    <>
+      <div className="todo-form">
+        <input
+          className="item-name"
+          value={item.text}
+          onChange={(e) =>
+            setItem({
+              id: item.id,
+              text: e.target.value,
+              completed: item.completed,
+            })
+          }
+        />
+        <button
+          className="item-button item-button-send"
+          onClick={() => addTask(item)}
+        >
+          <i className="fas fa-plus"></i> Agregar
+        </button>
+        <button
+          title="Limpiar"
+          className="item-button"
+          onClick={() => clearTask()}
+        >
+          <i className="fas fa-sync-alt"></i>
+        </button>
+      </div>
+      <hr></hr>
+      <div className="todo-list">
+        {tasks.map((task) => (
+          <TodoItem
+            key={task.id}
+            task={task}
+            editTask={editTask}
+            deleteTask={deleteTask}
+            toggleCompleted={toggleCompleted}
+          />
+        ))}
+      </div>
+    </>
+  );
 }
 export default TodoList;
